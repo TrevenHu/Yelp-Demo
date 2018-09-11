52a28949bcdbd6cfaf9166f8ba4a60fa9ff608ff
TrevenHu <hzy1994@bupt.edu.cn>
2018-09-11 01:25:01 -0700

    debug

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAIJECMQYR7RWTA2IA',                        # required unless using use_iam_profile
-    aws_secret_access_key: '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',
+    aws_access_key_id:     '',                        # required unless using use_iam_profile
+    aws_secret_access_key: '',
     region:                'us-west-1',                  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'yelpzoey'                                      # required
