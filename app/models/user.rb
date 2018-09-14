56d191d21b5f8db4bab8d13bfb4e739b1ac04d69
TrevenHu <hzy1994@bupt.edu.cn>
2018-09-13 21:57:07 -0700

    Link Reviews to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+ # user can have many reviews, and if user is deleted, all reviews will be deleted
+  has_many :reviews, dependent: :destroy
 end
