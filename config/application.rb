91c315cc848ba177d46ac5fadd19fdb15d9a3a4a
TrevenHu <hzy1994@bupt.edu.cn>
2018-09-20 18:13:06 -0700

    tell Rails not to bootstrap application during asset compilation

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Yelpdemo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    # tell Rails not to bootstrap your application during asset compilation
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
